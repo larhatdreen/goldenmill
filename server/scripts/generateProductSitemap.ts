e9c385bd93f02abc5aaa4024caa959d0c51609d3
larhatdreen <larhatdreen@yandex.ru>
2025-05-14 22:12:56 +0700

    add DATABASE_URL in secret

--- server/scripts/generateProductSitemap.ts
+++ server/scripts/generateProductSitemap.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import { PrismaClient } from '@prisma/client';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const prisma = new PrismaClient();
 
