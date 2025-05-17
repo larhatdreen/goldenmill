c3def08aeb94f46ff9a4989575b9c738e5db7d17
larhatdreen <larhatdreen@yandex.ru>
2025-05-18 02:08:21 +0700

    large add SSR 5.0

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 3005
       },
       max_memory_restart: '1G',
       exp_backoff_restart_delay: 100
