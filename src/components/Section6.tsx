82479de0dae6f77df37672b30949d186059fc70a
larhatdreen <larhatdreen@yandex.ru>
2025-05-21 12:57:31 +0700

    fix text on 6section

--- src/components/Section6.tsx
+++ src/components/Section6.tsx
@@ -57,7 +57,7 @@ function Section4() {
                 {/* SVG-блок */}
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="846" height="910" viewBox="0 0 846 1400" fill="none">
-                      <foreignObject width={340} height={70} x={280} y={0}>
+                      <foreignObject width={360} height={70} x={260} y={0}>
                         <div
                           className='font-["Bebas_Neue"] text-[32px] uppercase' 
                           style={{ color: isDark ? '#767676' : '#898989', lineHeight: '100%', letterSpacing: '3px' }}
